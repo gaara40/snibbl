6458b18f83b86643ee72eecad5683b8b6958f113
gaara40 <kumarpalukuru40@gmail.com>
2025-09-10 23:27:40 +0530

    changed the file name

---
+++ lib/helpers/on_tap_save_btn.dart
@@ -0,0 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+void onTapSaveBtn(BuildContext context, bool isSaved, String postId) async {
+  final currentUser = FirebaseAuth.instance.currentUser;
+  final userRef = FirebaseFirestore.instance
+      .collection('users')
+      .doc(currentUser!.uid);
+
+  if (isSaved) {
+    await userRef.update({
+      'savedPosts': FieldValue.arrayRemove([postId]),
+    });
+  } else {
+    await userRef.update({
+      'savedPosts': FieldValue.arrayUnion([postId]),
+    });
+  }
+}

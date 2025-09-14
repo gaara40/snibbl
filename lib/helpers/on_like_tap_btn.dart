4eef490b4250b5bc787ddaefe1dace7ce1f21c01
gaara40 <kumarpalukuru40@gmail.com>
2025-09-14 14:42:12 +0530

    Added likes messages to show in the activity screeen which user liked the post

--- lib/helpers/on_like_tap_btn.dart
+++ lib/helpers/on_like_tap_btn.dart
@@ -6,16 +6,60 @@ void onLikeTapBtn(BuildContext context, bool isLiked, String postId) async {
   final currentUser = FirebaseAuth.instance.currentUser;
 
   final postRef = FirebaseFirestore.instance.collection('posts').doc(postId);
+  final postSnap = await postRef.get();
+
+  if (!postSnap.exists) return;
+
+  //Post Owner ID
+  final postOwnerId = postSnap['userId'];
+
+  //Current user ID
+  final currentUserId = currentUser!.uid;
+
+  final currentUserDoc =
+      await FirebaseFirestore.instance
+          .collection('users')
+          .doc(currentUserId)
+          .get();
+
+  //Current User's username
+  final currentUsername = currentUserDoc['username'];
+  debugPrint('Current Username = $currentUsername');
+
+  //Activities Doc ID
+  final activitesDocId = '${postId}_$currentUserId';
 
   if (isLiked) {
     // unlike
     await postRef.update({
-      'likes': FieldValue.arrayRemove([currentUser!.email]),
+      'likes': FieldValue.arrayRemove([currentUser.email]),
     });
+    await FirebaseFirestore.instance
+        .collection('users')
+        .doc(postOwnerId)
+        .collection('activities')
+        .doc(activitesDocId)
+        .delete();
   } else {
     // like
     await postRef.update({
-      'likes': FieldValue.arrayUnion([currentUser!.email]),
+      'likes': FieldValue.arrayUnion([currentUser.email]),
     });
+
+    if (postOwnerId != currentUserId) {
+      await FirebaseFirestore.instance
+          .collection('users')
+          .doc(postOwnerId)
+          .collection('activities')
+          .doc(activitesDocId) //DETERMINISTIC ID
+          .set({
+            'type': 'like',
+            'fromUserId': currentUserId,
+            'fromUsername': currentUsername,
+            'postId': postId,
+            'message': '$currentUsername liked you post',
+            'createAt': FieldValue.serverTimestamp(),
+          });
+    }
   }
 }

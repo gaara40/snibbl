3004a616367511f1a62c551175c2e2f9f6092d23
gaara40 <kumarpalukuru40@gmail.com>
2025-09-20 23:57:41 +0530

    Extracted MySnibblsTab into a reusable widget for cleaner code

---
+++ lib/widgets/my_snibbls_tab.dart
@@ -0,0 +1,43 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:storygram/widgets/posts_item.dart';
+
+class MySnibblsTab extends StatelessWidget {
+  const MySnibblsTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    //Current User
+    final currentUser = FirebaseAuth.instance.currentUser;
+
+    //Current User's UID
+    final userId = currentUser?.uid;
+
+    final userPostsQuery = FirebaseFirestore.instance
+        .collection('posts')
+        .where('userId', isEqualTo: userId)
+        .orderBy('createdAt', descending: true);
+    return StreamBuilder(
+      stream: userPostsQuery.snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
+
+        final postDocs = snapshot.data!.docs;
+
+        return ListView.separated(
+          itemBuilder: (context, index) {
+            final postId = postDocs[index].id;
+            return PostsItem(onTap: () {}, postId: postId);
+          },
+          separatorBuilder: (BuildContext context, int index) {
+            return Padding(padding: EdgeInsets.all(4));
+          },
+          itemCount: postDocs.length,
+        );
+      },
+    );
+  }
+}

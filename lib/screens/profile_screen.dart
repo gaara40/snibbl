63e282ce4d5d8d8ad480854a9eab4a2f7e0751f2
gaara40 <kumarpalukuru40@gmail.com>
2025-09-21 16:11:57 +0530

    Fixed padding and removed saved posts icon from app bar, displayed posts count via reusable widgets

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:persistent_bottom_nav_bar/persistent_bottom_nav_bar.dart';
 import 'package:storygram/helpers/logout_dialog.dart';
 import 'package:storygram/helpers/toasts.dart';
+import 'package:storygram/widgets/user_posts_count.dart';
 import 'package:storygram/main.dart';
 import 'package:storygram/global_providers/auth_providers.dart';
 import 'package:storygram/themes/app_theme.dart';
@@ -10,7 +10,7 @@ import 'package:storygram/widgets/app_bar_logo.dart';
 import 'package:storygram/widgets/my_snibbls_tab.dart';
 import 'package:storygram/widgets/profile_posts_tab_item.dart';
 import 'package:storygram/widgets/saved_posts_tab.dart';
-import 'package:storygram/widgets/saved_posts_widget.dart';
+import 'package:storygram/widgets/user_saved_posts_count.dart';
 import 'package:storygram/widgets/username_text_widget.dart';
 
 class ProfileScreen extends ConsumerStatefulWidget {
@@ -60,25 +60,11 @@ class _ProfileScreenState extends ConsumerState<ProfileScreen> {
 
                 Spacer(),
 
-                //Saved Posts and Logout Button
+                //Logout Button
                 Padding(
-                  padding: const EdgeInsets.fromLTRB(12, 12, 12, 0),
+                  padding: const EdgeInsets.only(top: 12),
                   child: Row(
                     children: [
-                      // Saved Posts Button
-                      IconButton(
-                        icon: Icon(Icons.bookmark, size: 28),
-                        onPressed: () {
-                          PersistentNavBarNavigator.pushNewScreen(
-                            context,
-                            screen: const SavedPostsWidget(),
-                            withNavBar: false,
-                            pageTransitionAnimation:
-                                PageTransitionAnimation.cupertino,
-                          );
-                        },
-                      ),
-
                       // Logout Button
                       IconButton(
                         icon: Icon(Icons.logout, size: 25),
@@ -131,15 +117,15 @@ class _ProfileScreenState extends ConsumerState<ProfileScreen> {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Column(children: [Text('10'), Text("Snibbl's")]),
+                  Column(children: [UserPostsCount(), Text("Snibbl's")]),
                   SizedBox(width: 40),
                   Container(width: 1, height: 30, color: AppTheme.primaryColor),
                   SizedBox(width: 40),
-                  Column(children: [Text('10'), Text("Liked")]),
+                  Column(children: [Text('0'), Text("Liked")]),
                   SizedBox(width: 40),
                   Container(width: 1, height: 30, color: AppTheme.primaryColor),
                   SizedBox(width: 40),
-                  Column(children: [Text('10'), Text("Saved")]),
+                  Column(children: [SavedPostsCount(), Text("Saved")]),
                 ],
               ),
             ),
@@ -205,7 +191,10 @@ class _ProfileScreenState extends ConsumerState<ProfileScreen> {
                         ? const MySnibblsTab()
                         : selectedIndexTab == 1
                         ? const Center(
-                          child: Text('Liked Posts will appear here..'),
+                          child: Text(
+                            'Liked Posts will appear here\n🚀 This feature will be available soon 🚀',
+                            textAlign: TextAlign.center,
+                          ),
                         )
                         : const SavedPostsTab(),
               ),

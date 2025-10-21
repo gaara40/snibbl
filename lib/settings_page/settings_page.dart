3d1e7462ed7bbb23c2055f52e36ae8bcb2458671
gaara40 <kumarpalukuru40@gmail.com>
2025-10-22 01:04:35 +0530

    Added edit overlays conditionally for editing username and bio of the user

--- lib/settings_page/settings_page.dart
+++ lib/settings_page/settings_page.dart
@@ -4,18 +4,27 @@ import 'package:storygram/global_providers/auth_providers.dart';
 import 'package:storygram/helpers/logout_dialog.dart';
 import 'package:storygram/helpers/toasts.dart';
 import 'package:storygram/main.dart';
+import 'package:storygram/settings_page/widgets.dart/edit_overlay.dart';
 import 'package:storygram/settings_page/widgets.dart/setting_tile_card.dart';
 import 'package:storygram/themes/app_theme.dart';
 import 'package:storygram/widgets/username_text_widget.dart';
 
-class SettingsPage extends ConsumerWidget {
+class SettingsPage extends ConsumerStatefulWidget {
   const SettingsPage({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<SettingsPage> createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends ConsumerState<SettingsPage> {
+  bool _showEditUsername = false;
+  bool _showEditBio = false;
+
+  @override
+  Widget build(BuildContext context) {
     final authServices = ref.read(authServiceProvider);
 
-    //Logout Logic
+    // Logout Logic
     Future<void> logoutLogic() async {
       try {
         await authServices.signOut();
@@ -28,90 +37,147 @@ class SettingsPage extends ConsumerWidget {
       }
     }
 
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: AppTheme.secondaryColor,
-        title: const Text(
-          "Settings",
-          style: TextStyle(fontWeight: FontWeight.bold),
-        ),
-        elevation: 6,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(10),
-        child: ListView(
-          children: [
-            SizedBox(height: 10),
-            // Section 1: Profile
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+    return Stack(
+      children: [
+        Scaffold(
+          appBar: AppBar(
+            backgroundColor: AppTheme.secondaryColor,
+            title: const Text(
+              "Settings",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            elevation: 6,
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(10),
+            child: ListView(
               children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 6),
-                  child: const Text(
-                    "Profile",
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                  ),
-                ),
                 const SizedBox(height: 10),
-                SettingTileCard(
-                  icon: Icons.person_outline,
-                  title: 'Username',
-                  subTitle: UsernameTextWidget(14, FontWeight.w500),
-                  trailingText: 'Edit',
-                  onTap: () {},
+                // Section 1: Profile
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.only(left: 6),
+                      child: Text(
+                        "Profile",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    SettingTileCard(
+                      icon: Icons.person_outline,
+                      title: 'Username',
+                      subTitle: UsernameTextWidget(14, FontWeight.w500),
+                      trailingText: 'Edit',
+                      onTap: () {
+                        setState(() {
+                          _showEditUsername = true;
+                        });
+                      },
+                    ),
+                    SettingTileCard(
+                      icon: Icons.info_outline,
+                      title: 'Bio',
+                      subTitle: const Text(
+                        'Your bio',
+                        style: TextStyle(fontStyle: FontStyle.italic),
+                      ),
+                      trailingText: 'Edit',
+                      onTap: () {
+                        setState(() {
+                          _showEditBio = true;
+                        });
+                      },
+                    ),
+                  ],
                 ),
-
-                SettingTileCard(
-                  icon: Icons.info_outline,
-                  title: 'Bio',
-                  subTitle: Text(
-                    'Your bio',
-                    style: TextStyle(fontStyle: FontStyle.italic),
-                  ),
-                  trailingText: 'Edit',
-                  onTap: () {},
+                const SizedBox(height: 20),
+                // Section 2: Account
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.only(left: 6),
+                      child: Text(
+                        "Account",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    SettingTileCard(
+                      icon: Icons.lock_outline,
+                      title: 'Change Password',
+                      onTap: () {},
+                    ),
+                    SettingTileCard(
+                      icon: Icons.delete_forever_outlined,
+                      title: 'Permanently Delete Account',
+                      onTap: () {},
+                    ),
+                    SettingTileCard(
+                      icon: Icons.logout_outlined,
+                      title: 'Logout',
+                      onTap: () => showLogoutDialog(context, logoutLogic),
+                    ),
+                  ],
                 ),
               ],
             ),
-
-            SizedBox(height: 20),
-
-            // Section 2: Account
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 6),
-                  child: const Text(
-                    "Account",
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                  ),
-                ),
-                const SizedBox(height: 10),
-
-                SettingTileCard(
-                  icon: Icons.lock_outline,
-                  title: 'Change Password',
-                  onTap: () {},
-                ),
-
-                SettingTileCard(
-                  icon: Icons.delete_forever_outlined,
-                  title: 'Permanently Delete Account',
-                  onTap: () {},
-                ),
-
-                SettingTileCard(
-                  icon: Icons.logout_outlined,
-                  title: 'Logout',
-                  onTap: () => showLogoutDialog(context, logoutLogic),
-                ),
-              ],
-            ),
-          ],
+          ),
         ),
-      ),
+
+        AnimatedSwitcher(
+          duration: Duration(milliseconds: 200),
+          switchInCurve: Curves.easeIn,
+          switchOutCurve: Curves.easeOut,
+          child:
+              //Username Editing Overlay
+              _showEditUsername
+                  ? EditOverlay(
+                    key: const ValueKey('edit_username_overlay'),
+
+                    onCancelTap: () {
+                      setState(() {
+                        _showEditUsername = false;
+                      });
+                    },
+                    onSave: (value) {
+                      debugPrint('Username is saved as: $value');
+                      setState(() {
+                        _showEditUsername = false;
+                      });
+                    },
+                    title: 'Edit Username',
+                    hintText: 'New Username',
+                  )
+                  //Bio Editing Overlay
+                  : _showEditBio
+                  ? EditOverlay(
+                    key: const ValueKey('edit_bio_overlay'),
+                    onCancelTap: () {
+                      setState(() {
+                        _showEditBio = false;
+                      });
+                    },
+                    onSave: (value) {
+                      debugPrint('Bio is saved as :$value');
+                      setState(() {
+                        _showEditBio = false;
+                      });
+                    },
+                    title: 'Edit Bio',
+                    hintText: 'New Bio',
+                  )
+                  : const SizedBox.shrink(key: ValueKey('edit_overlay_empty')),
+        ),
+      ],
     );
   }
 }

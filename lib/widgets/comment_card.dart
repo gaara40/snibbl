42cb801e0fcfc3fe97b634e3b3826393852cabe9
gaara40 <kumarpalukuru40@gmail.com>
2025-11-05 13:53:25 +0530

    Generated CircleAvatar from username initial for cleaner code and reduced dependency on parent

--- lib/widgets/comment_card.dart
+++ lib/widgets/comment_card.dart
@@ -1,34 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:storygram/global_providers/user_provider.dart';
 
-class CommentCard extends StatelessWidget {
-  const CommentCard({
-    super.key,
-    required this.username,
-    required this.avatar,
-    required this.comment,
-  });
+class CommentCard extends ConsumerWidget {
+  const CommentCard({super.key, required this.userId, required this.comment});
 
-  final String username;
-  final String avatar;
+  final String userId;
+
   final String comment;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final theme = Theme.of(context);
-    return ListTile(
-      dense: true,
-      leading: CircleAvatar(radius: 18, child: Text(avatar)),
-      title: Text(
-        username,
-        style: theme.textTheme.bodyMedium!.copyWith(
-          fontSize: 14,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-      subtitle: Text(
-        comment,
-        style: theme.textTheme.bodyMedium!.copyWith(fontSize: 13),
-      ),
+
+    final userAsync = ref.watch(userProvider(userId));
+
+    return userAsync.when(
+      data: (userDoc) {
+        final userData = userDoc.data() as Map<String, dynamic>?;
+        final username = userData?['username'] ?? 'unknown_user';
+
+        return ListTile(
+          dense: true,
+          leading: CircleAvatar(radius: 18, child: Text(username[0])),
+          title: Text(
+            username,
+            style: theme.textTheme.bodyMedium!.copyWith(
+              fontSize: 14,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          subtitle: Text(
+            comment,
+            style: theme.textTheme.bodyMedium!.copyWith(fontSize: 13),
+          ),
+        );
+      },
+      loading: () => const ListTile(title: Text("Loading...")),
+      error: (e, _) => ListTile(title: Text("Error: $e")),
     );
   }
 }

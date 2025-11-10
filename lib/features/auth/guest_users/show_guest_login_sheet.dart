31f638c043ca40c0d4d2ac88864de2e02ad2da7d
gaara40 <kumarpalukuru40@gmail.com>
2025-11-11 01:17:05 +0530

    Integrated GuestAuthSheet widget into showGuestLoginSheet for guest access

---
+++ lib/features/auth/guest_users/show_guest_login_sheet.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:storygram/features/auth/presentation/widgets/guest_auth_sheet.dart';
+
+Future<void> showGuestLoginSheet(BuildContext context) async {
+  await showModalBottomSheet(
+    context: context,
+    useRootNavigator: true,
+    builder: (context) => GuestAuthSheet(),
+  );
+}

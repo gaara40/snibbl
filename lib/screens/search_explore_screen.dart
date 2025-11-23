2c0bb7a2f865883ab8add8ec29e0224aecccf899
gaara40 <kumarpalukuru40@gmail.com>
2025-11-24 01:23:27 +0530

    Refactored SearchExploreScreen layout to include a guest login button

--- lib/screens/search_explore_screen.dart
+++ lib/screens/search_explore_screen.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:storygram/features/auth/guest_users/show_guest_login_sheet.dart';
 
 class SearchExploreScreen extends StatelessWidget {
   const SearchExploreScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('This is Search Section')));
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('This is Search Section'),
+            ElevatedButton(
+              onPressed: () {
+                showGuestLoginSheet(context);
+              },
+              child: Text('Guest Login'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }

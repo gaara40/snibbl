4652c953565d805394e2beeadc4f4f1f61de9dbc
gaara40 <kumarpalukuru40@gmail.com>
2025-11-24 01:18:37 +0530

    Implemented email/password login/signup functionality for guest users in helper function

---
+++ lib/features/auth/helpers/handle_email_pass_login.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:storygram/core/helpers/toasts.dart';
+import 'package:storygram/features/auth/providers/auth_providers.dart';
+import 'package:storygram/main.dart';
+
+Future<void> handleEmailPassLogin({
+  required BuildContext context,
+  required WidgetRef ref,
+  required String email,
+  required String password,
+  required VoidCallback onStart,
+  required VoidCallback onEnd,
+}) async {
+  //Start Loading
+  onStart();
+
+  //try logging in
+  try {
+    final authServices = ref.watch(authServiceProvider);
+    final userCred = await authServices.logInWithEmailGuest(email, password);
+
+    if (userCred != null && userCred.user != null) {
+      final email = authServices.currentUserEmail;
+      showToast('Welcome $email');
+    }
+    await Future.delayed(Duration(seconds: 2));
+
+    if (context.mounted) {
+      navigatorKey.currentState!.pushNamedAndRemoveUntil(
+        '/mainScreen',
+        (route) => false,
+      );
+    }
+  } catch (e) {
+    //catch firebase errors
+    if (context.mounted) {
+      final errorMessage = e is Exception
+          ? e.toString().replaceFirst('Exception: ', '')
+          : e.toString();
+
+      showToast(errorMessage);
+    }
+  } finally {
+    onEnd();
+  }
+}
+
+Future<void> handleEmailPassSignup({
+  required BuildContext context,
+  required WidgetRef ref,
+  required String email,
+  required String password,
+  required String username,
+  required VoidCallback onStart,
+  required VoidCallback onEnd,
+}) async {
+  onStart();
+
+  try {
+    final authServices = ref.watch(authServiceProvider);
+    final userCred = await authServices.signUpGuest(email, password, username);
+
+    if (userCred.user != null) {
+      final emailAddr = authServices.currentUserEmail;
+      showToast('Welcome $emailAddr');
+    }
+
+    await Future.delayed(const Duration(seconds: 2));
+
+    if (context.mounted) {
+      navigatorKey.currentState!.pushNamedAndRemoveUntil(
+        '/mainScreen',
+        (route) => false,
+      );
+    }
+  } catch (e) {
+    if (context.mounted) {
+      final errorMessage = e is Exception
+          ? e.toString().replaceFirst('Exception: ', '')
+          : e.toString();
+      showToast(errorMessage);
+    }
+  } finally {
+    onEnd();
+  }
+}
